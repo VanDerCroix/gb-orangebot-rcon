42100c632d839785c93b7a62bf872619e1b2c065
vandercroix <pebcc12@gmail.com>
2018-10-05 23:57:44 -0500

    simpler listener

--- listener.js
+++ listener.js
@@ -38,203 +38,95 @@ s.on('message', function (msg, info) {
 	}
 	console.log(text);
 
-	// connected
-	re = named(/"(:<user_name>.+)[<](:<user_id>\d+)[>][<](:<steam_id>.*)[>]<>" entered the game/);
-	match = re.exec(text);
-	if (match !== null) {
-		if (match.capture('steam_id') != 'BOT') {
-			var conGameId = match.capture('user_id')
-			var conName = match.capture('user_name');
-			var conId = match.capture('steam_id');
-			var conId64 = id64(conId);
-			console.log('real player connected: ' + conName + ' gameId: ' + conGameId);
-		}
-	}
-
-	// join team
-	re = named(/"(:<user_name>.+)[<](:<user_id>\d+)[>][<](:<steam_id>.*)[>]" switched from team [<](:<user_team>CT|TERRORIST|Unassigned|Spectator)[>] to [<](:<new_team>CT|TERRORIST|Unassigned|Spectator)[>]/);
-	match = re.exec(text);
-	if (match !== null) {
-		if (servers[addr].state.players[match.capture('steam_id')] === undefined) {
-			if (match.capture('steam_id') != 'BOT') {
-				servers[addr].state.players[match.capture('steam_id')] = new Player(match.capture('steam_id'), match.capture('new_team'), match.capture('user_name'), undefined);
-			}
-		} else {
-			servers[addr].state.players[match.capture('steam_id')].steamid = match.capture('steam_id');
-			servers[addr].state.players[match.capture('steam_id')].team = match.capture('new_team');
-			servers[addr].state.players[match.capture('steam_id')].name = match.capture('user_name');
-		}
-		servers[addr].lastlog = +new Date();
-	}
-
-	// clantag
-	re = named(/"(:<user_name>.+)[<](:<user_id>\d+)[>][<](:<steam_id>.*?)[>][<](:<user_team>CT|TERRORIST|Unassigned|Spectator)[>]" triggered "clantag" \(value "(:<clan_tag>.*)"\)/);
-	match = re.exec(text);
-	if (match !== null) {
-		if (servers[addr].state.players[match.capture('steam_id')] === undefined) {
-			if (match.capture('steam_id') != 'BOT') {
-				servers[addr].state.players[match.capture('steam_id')] = new Player(match.capture('steam_id'), match.capture('user_team'), match.capture('user_name'), match.capture('clan_tag'));
-			}
-		} else {
-			servers[addr].state.players[match.capture('steam_id')].clantag = match.capture('clan_tag') !== '' ? match.capture('clan_tag') : undefined;
-		}
-		servers[addr].lastlog = +new Date();
-	}
-
-	// disconnect
-	re = named(/"(:<user_name>.+)[<](:<user_id>\d+)[>][<](:<steam_id>.*)[>][<](:<user_team>CT|TERRORIST|Unassigned|Spectator)[>]" disconnected/);
-	match = re.exec(text);
-	if (match !== null) {
-		if (servers[addr].state.players[match.capture('steam_id')] !== undefined) {
-			delete servers[addr].state.players[match.capture('steam_id')];
-		}
-		servers[addr].lastlog = +new Date();
-	}
-
-	// map loading
-	re = named(/Loading map "(:<map>.*?)"/);
-	match = re.exec(text);
-	if (match !== null) {
-		for (var prop in servers[addr].state.playerrs) {
-			if (servers[addr].state.players.hasOwnProperty(prop)) {
-				delete servers[addr].state.players[prop];
-			}
-		}
-		servers[addr].lastlog = +new Date();
-	}
-
-	// map started
-	re = named(/Started map "(:<map>.*?)"/);
-	match = re.exec(text);
-	if (match !== null) {
-		servers[addr].newmap(match.capture('map'));
-		servers[addr].lastlog = +new Date();
-	}
-
-	// round start
-	re = named(/World triggered "Round_Start"/);
-	match = re.exec(text);
-	if (match !== null) {
-		servers[addr].round();
-		servers[addr].lastlog = +new Date();
-	}
-
-	// round end
-	re = named(/Team "(:<team>.*)" triggered "SFUI_Notice_(:<team_win>Terrorists_Win|CTs_Win|Target_Bombed|Target_Saved|Bomb_Defused)" \(CT "(:<ct_score>\d+)"\) \(T "(:<t_score>\d+)"\)/);
-	match = re.exec(text);
-	if (match !== null) {
-		var score = {
-			'TERRORIST': parseInt(match.capture('t_score')),
-			'CT': parseInt(match.capture('ct_score'))
-		};
-		servers[addr].score(score);
-		servers[addr].lastlog = +new Date();
-	}
-
-	// !command
-	re = named(/"(:<user_name>.+)[<](:<user_id>\d+)[>][<](:<steam_id>.*)[>][<](:<user_team>CT|TERRORIST|Unassigned|Spectator|Console)[>]" say(:<say_team>_team)? "[!\.](:<text>.*)"/);
-	match = re.exec(text);
-	if (match !== null) {
-		var isadmin = match.capture('user_id') == '0' || servers[addr].admin(match.capture('steam_id'));
-		param = match.capture('text').split(' ');
-		cmd = param[0];
-		param.shift();
-		switch (String(cmd)) {
-		case 'admin':
-			break;
-		case 'restore':
-		case 'replay':
-			if (isadmin) servers[addr].restore(param);
-			break;
-		case 'status':
-		case 'stats':
-		case 'score':
-		case 'scores':
-			servers[addr].stats(true);
-			break;
-		case 'restart':
-		case 'reset':
-		case 'warmup':
-			if (isadmin) servers[addr].warmup();
-			break;
-		case 'maps':
-		case 'map':
-		case 'start':
-		case 'match':
-		case 'startmatch':
-			if (isadmin || !servers[addr].get().live) servers[addr].start(param);
-			break;
-		case 'force':
-			if (isadmin) servers[addr].ready(true);
-			break;
-		case 'resume':
-		case 'ready':
-		case 'rdy':
-		case 'unpause':
-			servers[addr].ready(match.capture('user_team'));
-			break;
-		case 'pause':
-			servers[addr].pause(match.capture('user_team'));
-			break;
-		case 'stay':
-			servers[addr].stay(match.capture('user_team'));
-			break;
-		case 'swap':
-		case 'switch':
-			servers[addr].swap(match.capture('user_team'));
-			break;
-		case 'knife':
-			servers[addr].knife();
-			break;
-		case 'disconnect':
-		case 'quit':
-		case 'leave':
-			if (isadmin) {
-				servers[addr].quit();
-				delete servers[addr];
-				console.log('Disconnected from ' + addr);
-			}
-			break;
-		case 'say':
-			if (isadmin) servers[addr].say(param.join(' '));
-			break;
-		case 'debug':
-			servers[addr].debug();
-			break;
-
-		case 'qwe':
-			servers[addr].rcon('script_execute welcome');
-			break;
-		case 'asd':
-			servers[addr].rcon('script welcome');
-			break;
-		case 'zxc':
-			servers[addr].rcon('script buttonReference <- Entities.FindByName( buttonReference, "'+match.capture('user_name')+'" );script ScriptPrintMessageChatAll(buttonReference.GetName())');
-			break;
-		case 'terro':
-			console.log(match.capture('user_name') + ' wants to go terro');
-			servers[addr].rcon('script sami <- Entities.FindByName( sami, "'+match.capture('user_name')+'" );script sami.SetTeam(2);');
-			break;
-		case 'tombo':
-			console.log(match.capture('user_name') + ' wants to go tombo');
-			servers[addr].rcon('script sami <- Entities.FindByName( sami, "'+match.capture('user_name')+'" );script ScriptPrintMessageChatAll(sami.GetName())');
-			break;
-		case 'mapname':
-			servers[addr].rcon('script ScriptPrintMessageChatAll(RandomInt().tostring());script ScriptPrintMessageChatAll(GetMapName())');
-			break;
-		case 'time':
-			servers[addr].rcon('script mape <- "holi";script ScriptPrintMessageChatAll(mape)');
-			break;
-		case 'fgh':
-			servers[addr].rcon('script mape <- "holi";script ScriptPrintMessageChatAll(mape)');
-			break;
-		default:
-		}
-		servers[addr].lastlog = +new Date();
-	}
 });
+
+function Player(steamid, team, name, clantag) {
+	this.steamid = steamid;
+	this.team = team;
+	this.name = name;
+	this.clantag = clantag;
+}
+function Server(address, pass, adminip, adminid, adminname) {
+	var tag = this;
+	this.state = {
+		ip: address.split(':')[0],
+		port: address.split(':')[1] || 27015,
+		pass: pass,
+		live: false,
+		map: '',
+		maps: [],
+		knife: false,
+		score: [],
+		knifewinner: false,
+		paused: false,
+		freeze: false,
+		unpause: {
+			'TERRORIST': false,
+			'CT': false
+		},
+		ready: {
+			'TERRORIST': false,
+			'CT': false
+		},
+		steamid: [],
+		admins: [],
+		queue: [],
+		players: {},
+		pauses: {}
+	};
+	this.realrcon = async function (cmd) {
+		if (cmd === undefined) return;
+		//console.log(cmd);
+		const conn = new rcon({
+		    host: this.state.ip,
+		    port: this.state.port
+		});
+		try {
+			await conn.authenticate(this.state.pass);
+			cmd = cmd.split(';');
+			for (var i in cmd) {
+				conn.execute(String(cmd[i]));
+			}
+			conn.disconnect()
+		} catch(e) {
+		    console.error(e);
+		}
+	};
+
+	this.realrcon('sv_rcon_whitelist_address ' + myip + ';logaddress_add ' + myip + ':' + myport + ';log on');
+	//this.status();
+
+	//s.send("plz go", 0, 6, this.state.port, this.state.ip); // SRCDS won't send data if it doesn't get contacted initially
+	console.log('Connected to ' + this.state.ip + ':' + this.state.port + ', pass ' + this.state.pass);
+}
 
 s.bind(myport);
 process.on('uncaughtException', function (err) {
 	console.log(err);
 });
+function initConnection() {
+	if(serverType == "local") myip = localIp;
+	
+	for (var i in server_config) {
+		if (server_config.hasOwnProperty(i)) {
+			addServer(server_config[i].host, server_config[i].port, server_config[i].pass);
+		}
+	}
+	console.log('OrangeBot listening on ' + myport);
+	console.log('Run this in CS console to connect or configure orangebot.js:');
+	console.log('connect YOUR_SERVER;password YOUR_PASS;rcon_password YOUR_RCON;rcon sv_rcon_whitelist_address ' + myip + ';rcon logaddress_add ' + myip + ':' + myport + ';rcon log on; rcon rcon_password '+rcon_pass+"\n");
+	
+	console.log('starting genbby server match');
+	//servers['142.93.81.233:27015'].start(['overpass', 'cache', 'mirage'])
+}
+
+function id64(steamid) {
+	return (new SteamID(String(steamid))).getSteamID64();
+}
+
+function addServer(host, port, pass) {
+	dns.lookup(host, 4, function (err, ip) {
+		servers[ip + ':' + port] = new Server(ip + ':' + port, pass);
+	});
+}
+initConnection()
